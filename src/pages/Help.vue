62c0a374052a8595e7fe7e9b1ec43b0ffa08152f
Diego <dpalaciosarriaga@gmail.com>
2019-11-28 18:45:42 -0600

    Estilos en Help

--- src/pages/Help.vue
+++ src/pages/Help.vue
@@ -1,6 +1,8 @@
 <template>
-    <q-page>
-        <h5>Help</h5>
+    <q-page class="q-pa-lg">
+        <h5 class="q-mt-none">Help</h5>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In sit labore, quos exercitationem consequuntur facere magnam ipsum molestiae at omnis voluptates cumque sint cupiditate facilis nisi officia, dignissimos nesciunt perferendis?</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In sit labore, quos exercitationem consequuntur facere magnam ipsum molestiae at omnis voluptates cumque sint cupiditate facilis nisi officia, dignissimos nesciunt perferendis?</p>
     </q-page>
 </template>
 
