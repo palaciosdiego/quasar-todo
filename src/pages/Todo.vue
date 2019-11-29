cd1e41caab6ddf1f1a487a39bbf564b9c09ee363
Diego <dpalaciosarriaga@gmail.com>
2019-11-28 18:46:11 -0600

    Se agrega lógica y elementos para mostrar eliminar tasks

--- src/pages/Todo.vue
+++ src/pages/Todo.vue
@@ -1,9 +1,78 @@
 <template>
-    <q-page>
-        <h5>Todo</h5>
+    <q-page class="bg-grey-3 column">
+        <q-list class="bg-white" separator bordered>
+            <q-item
+                v-for="(task, index) in tasks"
+                :key="task.title"
+                @click="task.done = !task.done"
+                :class="{ 'done bg-blue-1' : task.done }"
+                clickable
+                v-ripple
+            >
+                <q-item-section avatar>
+                    <q-checkbox v-model="task.done" class="no-pointer-events" color="primary" />
+                </q-item-section>
+                <q-item-section>
+                    <q-item-label>{{ task.title }}</q-item-label>
+                </q-item-section>
+                <q-item-section v-if="task.done" side>
+                    <q-btn
+                        @click.stop="deleteTask(index)"
+                        flat
+                        round
+                        dense
+                        color="primary"
+                        icon="delete"
+                    />
+                </q-item-section>
+            </q-item>
+        </q-list>
     </q-page>
 </template>
 
 <script>
-export default {};
+export default {
+    data() {
+        return {
+            tasks: [
+                {
+                    title: "Get bananas",
+                    done: false
+                },
+                {
+                    title: "Eat bananas",
+                    done: false
+                },
+                {
+                    title: "Poo bananas",
+                    done: false
+                }
+            ]
+        };
+    },
+    methods: {
+        deleteTask(index) {
+            this.$q
+                .dialog({
+                    title: "Confirm",
+                    message: "Realy delete?",
+                    cancel: true,
+                    persistent: true
+                })
+                .onOk(() => {
+                    console.log("delete task", index);
+                    this.tasks.splice(index, 1);
+                    this.$q.notify("Task deleted");
+                });
+        }
+    }
+};
 </script>
+<style lang="scss">
+.done {
+    .q-item__label {
+        text-decoration: line-through;
+        color: #bbb;
+    }
+}
+</style>
